718736db4ba5e3c3c5bd3ffeb0881b9f29d46cdf
Lektor <denislour@gmail.com>
2021-11-16 19:59:54 +0700

    Handle perform onSuccess and onError callback.

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,6 +2,14 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 export const RQSuperHeroesPage = () => {
+  const onSuccess = (data) => {
+    console.log("Perform side effect after data fetching", data);
+  };
+
+  const onError = (error) => {
+    console.log("Perform side effect after encountering error", error);
+  };
+
   const fetchSuperHeroes = () => {
     return axios.get("http://localhost:4000/superheroes");
   };
@@ -17,6 +25,12 @@ export const RQSuperHeroesPage = () => {
       // refetchInterval: 2000,
 
       enabled: false,
+
+      // On success callback
+      onSuccess,
+
+      // On error callback
+      onError,
     }
   );
 

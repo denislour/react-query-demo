a8e61cded0f658ad758a25c5b5e88c464678741c
Lektor <denislour@gmail.com>
2021-11-16 17:43:33 +0700

    Refactor fetch api from react hook to react query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,22 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
 export const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroes Page</div>;
+  const fetchSuperHeroes = () => {
+    return axios.get("http://localhost:4000/superheroes");
+  };
+
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Is Loading ...</h2>;
+  }
+  return (
+    <div>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </div>
+  );
 };
